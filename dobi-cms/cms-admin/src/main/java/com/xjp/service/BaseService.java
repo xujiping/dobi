b884ee3a6cca4f775f57f17bd6f1d579f59006c0
xujiping <xjpdyx@hotmail.com>
2017-09-19 17:29:28 +0800

    修改通用service，使其支持tk.mappers

--- dobi-cms/cms-admin/src/main/java/com/xjp/service/BaseService.java
+++ dobi-cms/cms-admin/src/main/java/com/xjp/service/BaseService.java
@@ -1,27 +1,46 @@
 package com.xjp.service;
 
-import org.apache.ibatis.annotations.Param;
-
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 /**
- * BaseService接口
+ * 通用Service接口.
  */
 public interface BaseService<Record, Example> {
 
+  List<Record> select(Record record);
+
+  Record selectByPrimaryKey(Long id);
+
+  List<Record> selectAll();
+
+  Record selectOne(Record record);
+
+  /**
+   * 会报NoSuchMethodException，原因不明.
+   * @param record model
+   * @return int
+   */
   int selectCount(Record record);
-
-  int deleteByExample(Example example);
-
-  int deleteByPrimaryKey(Integer id);
 
   int insert(Record record);
 
   int insertSelective(Record record);
 
-  List<Record> selectByExample(Example example);
+  int updateByPrimaryKeySelective(Record record);
 
-  List<Record> selectAll();
+  int updateByPrimaryKey(Record record);
+
+  int delete(Record record);
+
+  int deleteByPrimaryKeys(String ids);
+
+  int deleteByExample(Example example);
+
+  int deleteByPrimaryKey(Integer id);
+
+  List<Record> selectByExample(Example example);
 
   List<Record> selectByExampleForStartPage(Example example, Integer pageNum, Integer pageSize);
 
@@ -29,17 +48,10 @@ public interface BaseService<Record, Example> {
 
   Record selectFirstByExample(Example example);
 
-  Record selectByPrimaryKey(Long id);
-
   int updateByExampleSelective(@Param("record") Record record, @Param("example") Example example);
 
   int updateByExample(@Param("record") Record record, @Param("example") Example example);
 
-  int updateByPrimaryKeySelective(Record record);
-
-  int updateByPrimaryKey(Record record);
-
-  int deleteByPrimaryKeys(String ids);
 
   void initMapper();
 
--- dobi-cms/cms-admin/src/main/java/com/xjp/service/impl/BaseServiceImpl.java
+++ dobi-cms/cms-admin/src/main/java/com/xjp/service/impl/BaseServiceImpl.java
@@ -6,32 +6,81 @@ import com.xjp.db.DynamicDataSource;
 import com.xjp.service.BaseService;
 import com.xjp.util.SpringContextUtil;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.ibatis.annotations.Param;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.annotations.Param;
+
 /**
- * 实现BaseService抽象类
+ * 实现BaseService抽象类.
  * Created by xujiping on 2017/01/07.
  */
-public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseService<Record, Example> {
+public abstract class BaseServiceImpl<Mapper, Record, Example>
+    implements BaseService<Record, Example> {
 
   public Mapper mapper;
+
+  @Override
+  public List<Record> select(Record record) {
+    try {
+      DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
+      Method countByRecord = mapper.getClass().getDeclaredMethod("select", record.getClass());
+      List<Record> result = (List<Record>) countByRecord.invoke(mapper, record);
+      return result;
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+    DynamicDataSource.clearDataSource();
+    return null;
+  }
+
+  @Override
+  public Record selectOne(Record record) {
+    try {
+      DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
+      Method countByRecord = mapper.getClass().getDeclaredMethod("selectOne", record.getClass());
+      Record result = (Record) countByRecord.invoke(mapper, record);
+      return result;
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+    DynamicDataSource.clearDataSource();
+    return null;
+  }
+
+  @Override
+  public int delete(Record record) {
+    try {
+      DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
+      Method countByRecord = mapper.getClass().getDeclaredMethod("delete", record.getClass());
+      Object result = countByRecord.invoke(mapper, record);
+      return Integer.parseInt(String.valueOf(result));
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+    DynamicDataSource.clearDataSource();
+    return 0;
+  }
 
   @Override
   public int selectCount(Record record) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
-      Method[] methods = mapper.getClass().getDeclaredMethods();
-      for (int i = 0; i < methods.length; i++) {
-        System.out.print(methods[i].getName());
-        System.out.print(methods[i].getAnnotatedReturnType());
-        System.out.println("\n");
-      }
       Method countByRecord = mapper.getClass().getDeclaredMethod("selectCount", record.getClass());
       Object result = countByRecord.invoke(mapper, record);
       return Integer.parseInt(String.valueOf(result));
@@ -47,10 +96,29 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   }
 
   @Override
+  public List<Record> selectAll() {
+    try {
+      DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
+      Method selectByExample = mapper.getClass().getDeclaredMethod("selectAll");
+      Object result = selectByExample.invoke(mapper);
+      return (List<Record>) result;
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    } catch (NoSuchMethodException e) {
+      e.printStackTrace();
+    }
+    DynamicDataSource.clearDataSource();
+    return null;
+  }
+
+  @Override
   public int deleteByExample(Example example) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method deleteByExample = mapper.getClass().getDeclaredMethod("deleteByExample", example.getClass());
+      Method deleteByExample = mapper.getClass().getDeclaredMethod("deleteByExample",
+          example.getClass());
       Object result = deleteByExample.invoke(mapper, example);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -68,7 +136,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public int deleteByPrimaryKey(Integer id) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey", id.getClass());
+      Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey",
+          id.getClass());
       Object result = deleteByPrimaryKey.invoke(mapper, id);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -104,7 +173,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public int insertSelective(Record record) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method insertSelective = mapper.getClass().getDeclaredMethod("insertSelective", record.getClass());
+      Method insertSelective = mapper.getClass().getDeclaredMethod("insertSelective",
+          record.getClass());
       Object result = insertSelective.invoke(mapper, record);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -122,7 +192,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public List<Record> selectByExample(Example example) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
-      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample",
+          example.getClass());
       Object result = selectByExample.invoke(mapper, example);
       return (List<Record>) result;
     } catch (IllegalAccessException e) {
@@ -137,10 +208,12 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   }
 
   @Override
-  public List<Record> selectByExampleForStartPage(Example example, Integer pageNum, Integer pageSize) {
+  public List<Record> selectByExampleForStartPage(Example example, Integer pageNum, Integer
+      pageSize) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
-      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example
+          .getClass());
       PageHelper.startPage(pageNum, pageSize, false);
       Object result = selectByExample.invoke(mapper, example);
       return (List<Record>) result;
@@ -159,7 +232,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public List<Record> selectByExampleForOffsetPage(Example example, Integer offset, Integer limit) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
-      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example
+          .getClass());
       PageHelper.offsetPage(offset, limit, false);
       Object result = selectByExample.invoke(mapper, example);
       return (List<Record>) result;
@@ -178,7 +252,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public Record selectFirstByExample(Example example) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
-      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example.getClass());
+      Method selectByExample = mapper.getClass().getDeclaredMethod("selectByExample", example
+          .getClass());
       List<Record> result = (List<Record>) selectByExample.invoke(mapper, example);
       if (null != result && result.size() > 0) {
         return result.get(0);
@@ -200,7 +275,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.SLAVE.getName());
       //因通用mapper形参类型是Object，所以此处获取Long的父类Number再获取父类Object
-      Method selectByPrimaryKey = mapper.getClass().getDeclaredMethod("selectByPrimaryKey", id.getClass().getSuperclass().getSuperclass());
+      Method selectByPrimaryKey = mapper.getClass().getDeclaredMethod("selectByPrimaryKey", id
+          .getClass().getSuperclass().getSuperclass());
       Object result = selectByPrimaryKey.invoke(mapper, id);
       return (Record) result;
     } catch (IllegalAccessException e) {
@@ -215,10 +291,12 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   }
 
   @Override
-  public int updateByExampleSelective(@Param("record") Record record, @Param("example") Example example) {
+  public int updateByExampleSelective(@Param("record") Record record, @Param("example") Example
+      example) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method updateByExampleSelective = mapper.getClass().getDeclaredMethod("updateByExampleSelective", record.getClass(), example.getClass());
+      Method updateByExampleSelective = mapper.getClass().getDeclaredMethod
+          ("updateByExampleSelective", record.getClass(), example.getClass());
       Object result = updateByExampleSelective.invoke(mapper, record, example);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -236,7 +314,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public int updateByExample(@Param("record") Record record, @Param("example") Example example) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method updateByExample = mapper.getClass().getDeclaredMethod("updateByExample", record.getClass(), example.getClass());
+      Method updateByExample = mapper.getClass().getDeclaredMethod("updateByExample", record
+          .getClass(), example.getClass());
       Object result = updateByExample.invoke(mapper, record, example);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -254,7 +333,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public int updateByPrimaryKeySelective(Record record) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method updateByPrimaryKeySelective = mapper.getClass().getDeclaredMethod("updateByPrimaryKeySelective", record.getClass());
+      Method updateByPrimaryKeySelective = mapper.getClass().getDeclaredMethod
+          ("updateByPrimaryKeySelective", record.getClass());
       Object result = updateByPrimaryKeySelective.invoke(mapper, record);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -272,7 +352,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   public int updateByPrimaryKey(Record record) {
     try {
       DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getName());
-      Method updateByPrimaryKey = mapper.getClass().getDeclaredMethod("updateByPrimaryKey", record.getClass());
+      Method updateByPrimaryKey = mapper.getClass().getDeclaredMethod("updateByPrimaryKey",
+          record.getClass());
       Object result = updateByPrimaryKey.invoke(mapper, record);
       return Integer.parseInt(String.valueOf(result));
     } catch (IllegalAccessException e) {
@@ -300,7 +381,8 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
           continue;
         }
         Integer id = Integer.parseInt(idStr);
-        Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey", id.getClass());
+        Method deleteByPrimaryKey = mapper.getClass().getDeclaredMethod("deleteByPrimaryKey", id
+            .getClass());
         Object result = deleteByPrimaryKey.invoke(mapper, id);
         count += Integer.parseInt(String.valueOf(result));
       }
@@ -322,10 +404,11 @@ public abstract class BaseServiceImpl<Mapper, Record, Example> implements BaseSe
   }
 
   /**
-   * 获取类泛型class
+   * 获取类泛型class.
    */
   public Class<Mapper> getMapperClass() {
-    return (Class<Mapper>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    return (Class<Mapper>) ((ParameterizedType) getClass().getGenericSuperclass())
+        .getActualTypeArguments()[0];
   }
 
 }
